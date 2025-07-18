461ba3d736d76ef6ecab7915ecf259b26e3522c2
WeiArango <152320369+WeiArango@users.noreply.github.com>
2025-07-17 23:42:32 -0500

    Update ProyectoFinal.py

--- ProyectoFinal.py
+++ ProyectoFinal.py
@@ -36,7 +36,7 @@ def load_model():
     with open('creditcard_model.pkl', 'rb') as f:
         return pickle.load(f)
 
-model = load_model()
+#model = load_model()
 
 # Dividir en columnas
 col1, col2 = st.columns([2, 3])
